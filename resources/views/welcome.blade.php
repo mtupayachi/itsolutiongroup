54266396fb7761f53779887dfe01bd4f4f62f891
iammalf <iammalf@gmail.com>
2018-12-04 03:16:30 -0500

    modificando home
    nuevas modificaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -238,9 +238,7 @@
             <!-- Section heading -->
             <h2 class="h1-responsive font-weight-bold text-center my-5">Nuestros Servicios</h2>
             <!-- Section description -->
-            <p class="lead grey-text text-center w-responsive mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam.</p>
+            <p class="lead grey-text text-center w-responsive mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <div class="row">
               <div class="col-md-4">
                 <!-- Card -->
